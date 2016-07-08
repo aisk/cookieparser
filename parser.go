de00953107ec225e84a1376a7f73dbe5689b17ee
asaka <aisk1988@gmail.com>
2016-07-08 17:36:40 +0800

    add ToString function

--- parser.go
+++ parser.go
@@ -2,12 +2,22 @@ package cookieparser
 
 import (
 	"net/http"
+	"strings"
 )
 
 // Parse raw cookie string
 func Parse(raw string) []*http.Cookie {
 	header := http.Header{}
-    header.Add("Cookie", raw)
-    request := http.Request{Header: header}
+	header.Add("Cookie", raw)
+	request := http.Request{Header: header}
 	return request.Cookies()
+}
+
+// ToString convert the cookies to a string
+func ToString(cookies []*http.Cookie) string {
+	var cookieStrings []string
+	for _, cookie := range cookies {
+		cookieStrings = append(cookieStrings, cookie.String())
+	}
+	return strings.Join(cookieStrings, ",")
 }
